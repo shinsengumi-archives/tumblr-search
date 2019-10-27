14e5c7bfb9780e08863b6086f91b4a7e4be91451
A. C. Yu <amyyu123@hotmail.com>
2019-10-27 00:58:02 -0700

    Combine data and tags json files

--- gen.py
+++ gen.py
@@ -8,12 +8,11 @@ NPAGES = 14
 
 root_url = "https://shinsengumi-archives.tumblr.com/"
 data = {}
-tags = {}
+data["posts"] = {}
+data["tags"] = {}
 if not CLEAN_RUN:
 	with open("data.json", "r") as read_file:
 		data = json.load(read_file)
-	with open("tags.json", "r") as read_file:
-		tags = json.load(read_file)
 
 for page_id in range(NPAGES, 12, -1):
 	page_url = root_url+"page/"+str(page_id)
@@ -22,13 +21,11 @@ for page_id in range(NPAGES, 12, -1):
 
 	post_urls = [url.get('href') for url in doc.cssselect('h3.post-title a')]
 	post_htmls = [html.tostring(raw).decode('utf-8') for raw in doc.cssselect('div.post-text')]
-	print(len(post_htmls), len(post_urls))
 
 	for post_url, post_html in zip(post_urls, post_htmls):
 
 		id = post_url[post_url.find("/post/")+6 : post_url.rfind("/")]
 		if id in data:
-			print(id)
 			continue
 
 		post = requests.get(post_url)
@@ -46,9 +43,9 @@ for page_id in range(NPAGES, 12, -1):
 			doc.cssselect('div.tags a.single-tag')
 		))
 		for tag in post_tags:
-			if tag not in tags:
-				tags[tag] = []
-			tags[tag].append(id)
+			if tag not in data["tags"]:
+				data["tags"][tag] = []
+			data["tags"][tag].append(id)
 
 		post_data = {
 			'url': post_url,
@@ -58,11 +55,8 @@ for page_id in range(NPAGES, 12, -1):
 			'tags': post_tags,
 		}
 		
-		data[id] = post_data
+		data["posts"][id] = post_data
 
 
 with open('data.json', 'w', encoding='utf8') as outfile:
     json.dump(data, outfile, sort_keys=True)
-	
-with open('tags.json', 'w', encoding='utf8') as outfile:
-    json.dump(tags, outfile, sort_keys=True)
--- search.js
+++ search.js
@@ -3,8 +3,10 @@ alert("hello");
 var keyword = "okita";
 var tags = [];
 
-$.getJSON('http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20WHERE%20symbol%3D%27WRC%27&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback', function(data) {
-    //data is the JSON string
+const data_file = 'https://raw.githubusercontent.com/shinsengumi-archives/tumblr-search/master/data.json';
+
+$.getJSON(data_file, function(data) {
+	console.log(data);
 });
 
 //window.location.replace("http://www.w3schools.com");
