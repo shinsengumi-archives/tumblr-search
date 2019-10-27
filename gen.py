32e4e38cb9eba88df4fba6b877d194967ab59eca
A. C. Yu <amyyu123@hotmail.com>
2019-10-27 05:38:04 -0700

    fix string encoding

--- gen.py
+++ gen.py
@@ -3,10 +3,8 @@ import requests
 import re
 import json
 
-CLEAN_RUN = False
+CLEAN_RUN = True
 NPAGES = 14
-
-print("沖".encode('utf8'))
 
 root_url = "https://shinsengumi-archives.tumblr.com/"
 data = {}
@@ -20,25 +18,23 @@ for page_id in range(NPAGES, 0, -1):
 	print("page", page_id);
 	page_url = root_url+"page/"+str(page_id)
 	page = requests.get(page_url)
-	doc = html.fromstring(page.content)
+	doc = html.fromstring(page.content.decode('utf8'))
 
 	post_urls = [url.get('href') for url in doc.cssselect('h3.post-title a')]
-	post_htmls = [str(html.tostring(raw, encoding="UTF-8")) for raw in doc.cssselect('div.post')]
+	post_htmls = [str(html.tostring(raw)) for raw in doc.cssselect('div.post')]
 
 	for post_url, post_html in zip(post_urls, post_htmls):
-		#post_url = "https://shinsengumi-archives.tumblr.com/post/188382973356/rintaro-okita-%E6%B2%96%E7%94%B0%E6%9E%97%E5%A4%AA%E9%83%8E-wiki-corpus"
-		#post_url = "https://shinsengumi-archives.tumblr.com/post/188360292966/soji-okita-%E6%B2%96%E7%94%B0%E7%B7%8F%E5%8F%B8-wiki-corpus"
 		
 		id = post_url[post_url.find("/post/")+6 : post_url.rfind("/")]
 		if id in data["posts"]:
 			continue
 
 		post = requests.get(post_url)
-		doc = html.fromstring(post.content)
+		doc = html.fromstring(post.content.decode('utf8'))
 
-		title = str(doc.cssselect('h3.post-title')[0].text_content().encode('utf8'))
+		title = str(doc.cssselect('h3.post-title')[0].text_content())
 
-		text = str(html.tostring(doc.cssselect('div.post-text')[0], encoding="UTF-8"))
+		text = str(html.tostring(doc.cssselect('div.post-text')[0]))
 		text = re.sub('<.*?>', ' ', text)
 		text = text.strip().replace('\r', ' ').replace('\n', ' ').replace('\t', ' ').replace(' ', ' ')
 		text = re.sub('[ ]{2,}', ' ', text)
--- search.js
+++ search.js
@@ -1,16 +1,18 @@
 
+console.log(decodeURIComponent(escape("(\xe6\xb2\x96\xe7\x94\xb0\xe7\xb7\x8f\xe5\x8f\xb8) [wiki-corpus]")));
+
 window.onload = function () {
     var iSearch = document.URL.indexOf("/search/");
     if(iSearch >= 0){
     	var keyword = document.URL.substring(iSearch+8);
-    	mySearch(keyword, ["-index"]);
+    	//mySearch(keyword, ["-index"]);
     }
 }
 
 function mySearch(keyword, tags){
 	const data_file = 'https://raw.githubusercontent.com/shinsengumi-archives/tumblr-search/master/data.json';
 
-	keyword = unescape(encodeURIComponent(keyword.toLowerCase()));
+	keyword = keyword.toLowerCase();
 	var tags_must_contain = tags.filter(function(tag){
 		return tag.charAt(0) == '+';
 	}).map(function(tag){
